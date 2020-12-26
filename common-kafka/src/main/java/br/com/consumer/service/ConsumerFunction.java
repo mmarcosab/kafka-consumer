b9ce10c7d26c13dfade82b2d12f74cf549f0d947
Marcos Vinicius <marcosab.vinicius@gmail.com>
2020-12-25 22:16:55 -0300

    Refatoração do projeto, estva feito de uma maneira muito primitiva

--- common-kafka/src/main/java/br/com/consumer/service/ConsumerFunction.java
+++
@@ -1,7 +0,0 @@
-package br.com.consumer.service;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction<T> {
-    void consume(ConsumerRecord<String, T> record);
-}
---
+++ src/main/java/br/com/example/broker/consumer/TopicListener.java
@@ -0,0 +1,28 @@
+package br.com.example.broker.consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TopicListener {
+
+    @Value("${topic.name.consumer")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+        log.info("Tópico: {}", topicName);
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+    }
+
+}
